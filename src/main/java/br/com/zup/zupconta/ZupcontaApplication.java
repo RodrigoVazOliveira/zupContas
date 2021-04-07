0e05f4e1676e6cef83b9d0b18cde5b1e4c2d628a
Rodrigo Vaz <rodrigovazdeoliveira@gmail.com>
2021-04-07 15:49:09 -0300

    Criar projeto com spring boot

---
+++ src/main/java/br/com/zup/zupconta/ZupcontaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.zupconta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupcontaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupcontaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/zupconta/ZupcontaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.zupconta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupcontaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
