8442202fda814799dec2d7678fa813309e10a918
Rodrigo Vaz <rodrigovazdeoliveira@gmail.com>
2021-04-07 17:21:01 -0300

    Criar modelo de categoria

---
+++ src/main/java/br/com/zup/zupnancas/models/Categoria.java
@@ -0,0 +1,50 @@
+package br.com.zup.zupnancas.models;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@CPF
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 50)
+    private String nome;
+
+    public Categoria() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(id, categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
