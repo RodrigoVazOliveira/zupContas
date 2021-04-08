fcca79feff4bdfc4cdf89d001a9509c90394ea72
Rodrigo Vaz <rodrigovazdeoliveira@gmail.com>
2021-04-08 13:14:41 -0300

    Criar controller para categoria

---
+++ src/main/java/br/com/zup/zupnancas/controllers/CategoriaController.java
@@ -0,0 +1,18 @@
+package br.com.zup.zupnancas.controllers;
+
+import br.com.zup.zupnancas.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("categorias/")
+public class CategoriaController {
+
+    private CategoriaService categoriaService;
+
+    @Autowired
+    public CategoriaController(CategoriaService categoriaService) {
+        this.categoriaService = categoriaService;
+    }
+}
