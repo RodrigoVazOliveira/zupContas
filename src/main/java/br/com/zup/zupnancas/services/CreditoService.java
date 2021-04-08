d2e0c17ee6bc650c65877f15ef80fd25c5ed02f8
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-08 08:38:20 -0300

    Criar método para gravar um novo credito

--- src/main/java/br/com/zup/zupnancas/services/CreditoService.java
+++ src/main/java/br/com/zup/zupnancas/services/CreditoService.java
@@ -1,16 +1,28 @@
 package br.com.zup.zupnancas.services;
 
+import br.com.zup.zupnancas.models.Credito;
 import br.com.zup.zupnancas.repositories.CreditoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class CreditoService {
 
     private final CreditoRepository creditoRepository;
+    private final SaldoService saldoService;
 
     @Autowired
-    public CreditoService(CreditoRepository creditoRepository) {
+    public CreditoService(CreditoRepository creditoRepository, SaldoService saldoService) {
         this.creditoRepository = creditoRepository;
+        this.saldoService = saldoService;
+    }
+
+    public Credito adicionarNovoCredito(Credito credito) {
+        credito.setDataDeEntrada(LocalDate.now());
+        Credito novoCredito = creditoRepository.save(credito);
+        saldoService.creditarSaldo(novoCredito);
+        return novoCredito;
     }
 }
