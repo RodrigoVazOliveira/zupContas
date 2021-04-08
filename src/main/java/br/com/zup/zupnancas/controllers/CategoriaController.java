a5afddbc6b774aff8af2ce9d5eeae8ed8e11d7bf
Rodrigo Vaz <rodrigovazdeoliveira@gmail.com>
2021-04-08 13:20:19 -0300

    Criar endpoint para cadastrar nova categoria

--- src/main/java/br/com/zup/zupnancas/controllers/CategoriaController.java
+++ src/main/java/br/com/zup/zupnancas/controllers/CategoriaController.java
@@ -1,9 +1,13 @@
 package br.com.zup.zupnancas.controllers;
 
+import br.com.zup.zupnancas.dto.categoria.CadastrarCategoriaDTO;
+import br.com.zup.zupnancas.models.Categoria;
 import br.com.zup.zupnancas.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("categorias/")
@@ -14,5 +18,13 @@ public class CategoriaController {
     @Autowired
     public CategoriaController(CategoriaService categoriaService) {
         this.categoriaService = categoriaService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Categoria gravarNovaCategoria(@RequestBody @Valid CadastrarCategoriaDTO cadastrarCategoriaDTO) {
+        return categoriaService.gravarNovaCategoria(
+                cadastrarCategoriaDTO.converterCadastrarCategoriaDtoParaCategoria()
+        );
     }
 }
