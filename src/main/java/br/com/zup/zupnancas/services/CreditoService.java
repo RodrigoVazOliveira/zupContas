a5c55936ca84de9686ad13ab5c601e81a952ce48
Rodrigo Vaz <rodrigovazdeoliveira@gmail.com>
2021-04-08 15:42:16 -0300

    Adicionar no cadastro de credito as categorias para gravar no banco de dados

--- src/main/java/br/com/zup/zupnancas/services/CreditoService.java
+++ src/main/java/br/com/zup/zupnancas/services/CreditoService.java
@@ -1,28 +1,48 @@
 package br.com.zup.zupnancas.services;
 
+import br.com.zup.zupnancas.models.Categoria;
 import br.com.zup.zupnancas.models.Credito;
 import br.com.zup.zupnancas.repositories.CreditoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CreditoService {
 
     private final CreditoRepository creditoRepository;
     private final SaldoService saldoService;
+    private final CategoriaService categoriaService;
 
     @Autowired
-    public CreditoService(CreditoRepository creditoRepository, SaldoService saldoService) {
+    public CreditoService(CreditoRepository creditoRepository, SaldoService saldoService, CategoriaService categoriaService) {
         this.creditoRepository = creditoRepository;
         this.saldoService = saldoService;
+        this.categoriaService = categoriaService;
     }
 
     public Credito adicionarNovoCredito(Credito credito) {
         credito.setDataDeEntrada(LocalDate.now());
+        cadastrarCategorias(credito.getCategoria());
         Credito novoCredito = creditoRepository.save(credito);
         saldoService.creditarSaldo(novoCredito);
         return novoCredito;
+    }
+
+    private void cadastrarCategorias(List<Categoria> categorias) {
+        List<Categoria> categoriasCadastradas = new ArrayList<>();
+        for (Categoria categoria : categorias) {
+            if (categoria.getId() == null) {
+                categoriasCadastradas.add(categoriaService.gravarNovaCategoria(categoria));
+            } else {
+                categoriasCadastradas.add(
+                        categoriaService.pesquisarCategoriaPorId(categoria)
+                );
+            }
+        }
+        categorias = categoriasCadastradas;
     }
 }
