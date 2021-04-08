17818e7e71a01d27c6a8b738a5ce429decfbbe21
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-08 09:48:15 -0300

    Criar método para cadastrar nova conta

--- src/main/java/br/com/zup/zupnancas/services/ContaService.java
+++ src/main/java/br/com/zup/zupnancas/services/ContaService.java
@@ -1,5 +1,7 @@
 package br.com.zup.zupnancas.services;
 
+import br.com.zup.zupnancas.enumerates.Status;
+import br.com.zup.zupnancas.models.Conta;
 import br.com.zup.zupnancas.repositories.ContaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,5 +14,16 @@ public class ContaService {
     @Autowired
     public ContaService(ContaRepository contaRepository) {
         this.contaRepository = contaRepository;
+    }
+
+    public Conta gravarNovaConta(Conta conta) {
+        verificarStatus(conta.getStatus());
+        return contaRepository.save(conta);
+    }
+
+    public void verificarStatus(Status status) {
+        if (status.equals(Status.PAGO)) {
+            throw new RuntimeException("O status deve ser apenas ARGUARDANDO OU ATRASADO!");
+        }
     }
 }
