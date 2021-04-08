819647781b0b9002eba1ef35b44d76ec46182837
Rodrigo Vaz <rodrigovazdeoliveira@gmail.com>
2021-04-08 13:07:25 -0300

    Criar serviço de categoria

--- src/main/java/br/com/zup/zupnancas/services/CategoriaService.java
+++ src/main/java/br/com/zup/zupnancas/services/CategoriaService.java
@@ -1,2 +1,14 @@
-package br.com.zup.zupnancas.services;public class CategoriaService {
+package br.com.zup.zupnancas.services;
+
+import br.com.zup.zupnancas.repositories.CategoriaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoriaService {
+
+    private CategoriaRepository categoriaRepository;
+
+    public CategoriaService(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
 }
