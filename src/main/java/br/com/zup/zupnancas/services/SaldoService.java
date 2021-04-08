398b433b4b9a4f79a3593790b3e7fbf42a66b3de
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-08 08:36:02 -0300

    Criar regra de negocio para atualizar saldo ao cadastrar um novo credito

--- src/main/java/br/com/zup/zupnancas/services/SaldoService.java
+++ src/main/java/br/com/zup/zupnancas/services/SaldoService.java
@@ -1,9 +1,12 @@
 package br.com.zup.zupnancas.services;
 
+import br.com.zup.zupnancas.models.Credito;
 import br.com.zup.zupnancas.models.Saldo;
 import br.com.zup.zupnancas.repositories.SaldoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class SaldoService {
@@ -17,5 +20,19 @@ public class SaldoService {
 
     public Saldo gravarNovoSaldo(Saldo saldo) {
         return saldoRepository.save(saldo);
+    }
+
+    public void creditarSaldo(Credito credito) {
+        Optional<Saldo> optionalSaldo = saldoRepository.findById(credito.getSaldo().getCpf());
+
+        if (optionalSaldo.isEmpty()) {
+            throw new RuntimeException("Não existe saldo com o CPF " + credito.getSaldo().getCpf());
+        }
+
+        Saldo saldo = optionalSaldo.get();
+        Double valorAtualizado = saldo.getValor() + credito.getValor();
+        saldo.setValor(valorAtualizado);
+
+        saldoRepository.save(saldo);
     }
 }
